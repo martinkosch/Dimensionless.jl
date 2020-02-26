d743b892dc4a4d87db8b9713903973cae4602282
Martin Kosch <martin.kosch@rwth-aachen.de>
2020-02-26 19:38:20 +0100

    Initial commit

--- src/Dimensionless.jl
+++ src/Dimensionless.jl
@@ -1,5 +1,20 @@
 module Dimensionless
 
-greet() = print("Hello World!")
+using Unitful, LinearAlgebra
+
+export basis_dimensions, dimensionless, dimensionful
+
+struct basis_dimensions
+    dimensions
+    dimensional_matrix
+end
+
+function dimensionless()
+
+end
+
+function dimensionful()
+
+end
 
 end # module
