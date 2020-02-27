2e1e91585ec0e7302f132c15ce6363a94f0cfc60
Martin Kosch <martin.kosch@rwth-aachen.de>
2020-02-27 21:16:13 +0100

    BasisQuantities first implementation

--- src/Dimensionless.jl
+++ src/Dimensionless.jl
@@ -2,18 +2,41 @@ module Dimensionless
 
 using Unitful, LinearAlgebra
 
-export basis_dimensions, dimensionless, dimensionful
+export BasisQuantities, dimensionless, dimensionful
 
-struct basis_dimensions
-    dimensions
-    dimensional_matrix
+struct BasisQuantities
+    quantities::Tuple
+    dimensions::Set
+    dimensional_matrix::Array{Rational}
+    function BasisQuantities(quantities::Vararg{Unitful.Quantity})
+        dimensions = Set{Type{<:Unitful.Dimension}}()
+        for quantity in quantities
+            union!(dimensions, typeof.(typeof(dimension(quantity)).parameters[1]))
+        end
+
+        dimensional_matrix = zeros(Rational, length(dimensions), length(quantities))
+        for (quantity_ind,quantity) in enumerate(quantities)
+            for quantity_dim in typeof(typeof(quantity).parameters[2]).parameters[1]
+                for (dim_ind,dim) in enumerate(dimensions)
+                    if isa(quantity_dim, dim)
+                        dimensional_matrix[dim_ind,quantity_ind] = quantity_dim.power
+                    end
+                end
+            end
+        end
+
+        if LinearAlgebra.rank(dimensional_matrix) != length(quantities)
+            error("Base dimensions are not linear independent!")
+        end
+        return new(quantities, dimensions, dimensional_matrix)
+    end
 end
 
-function dimensionless()
+function dimensionless(quantity::Unitful.Quantity, basis_quantities::BasisQuantities)
 
 end
 
-function dimensionful()
+function dimensionful(quantity::Union{Unitful.NoUnits, Number}, unit::Unitful.Units, basis_quantities::BasisQuantities)
 
 end
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,8 @@
-using Dimensionless
+using Dimensionless, Unitful
 using Test
 
-@testset "Dimensionless.jl" begin
-    # Write your own tests here.
+basis = BasisQuantities(9.81u"m/s^2", 6371u"km", 1420788u"kg", 1u"mol/g")
+
+@testset "Basic tests" begin
+    @test basis.dimensional_matrix == Rational.([-2 0 0 0; 1 1 0 0; 0 0 0 1; 0 0 1 -1])
 end
