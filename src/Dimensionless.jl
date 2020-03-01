368d5606fc58777c9f72fd20baa501adc3989014
Martin Kosch <martin.kosch@rwth-aachen.de>
2020-03-01 15:03:24 +0100

    Renaming BasisQuantities to DimensionBasis;  Implementation of functions

--- src/Dimensionless.jl
+++ src/Dimensionless.jl
@@ -2,42 +2,54 @@ module Dimensionless
 
 using Unitful, LinearAlgebra
 
-export BasisQuantities, dimensionless, dimensionful
+export DimensionBasis, dimensional_matrix, dimensionless, dimensionful
 
-struct BasisQuantities
-    quantities::Tuple
-    dimensions::Set
-    dimensional_matrix::Array{Rational}
-    function BasisQuantities(quantities::Vararg{Unitful.Quantity})
-        dimensions = Set{Type{<:Unitful.Dimension}}()
-        for quantity in quantities
-            union!(dimensions, typeof.(typeof(dimension(quantity)).parameters[1]))
-        end
+struct DimensionBasis
+    basis_vectors
+    dim_set
+    dim_mat
 
-        dimensional_matrix = zeros(Rational, length(dimensions), length(quantities))
-        for (quantity_ind,quantity) in enumerate(quantities)
-            for quantity_dim in typeof(typeof(quantity).parameters[2]).parameters[1]
-                for (dim_ind,dim) in enumerate(dimensions)
-                    if isa(quantity_dim, dim)
-                        dimensional_matrix[dim_ind,quantity_ind] = quantity_dim.power
-                    end
-                end
-            end
-        end
+    function DimensionBasis(basis_vectors::Vararg{Unitful.AbstractQuantity})
+        dim_set = unique_dimensions(basis_vectors...)
+        dim_mat = dimensional_matrix(dim_set, basis_vectors...)
 
-        if LinearAlgebra.rank(dimensional_matrix) != length(quantities)
-            error("Base dimensions are not linear independent!")
+        if size(dim_mat, 2) < size(dim_mat, 1)
+            error("Too few basis vectors!")
+        elseif LinearAlgebra.rank(dim_mat) != length(basis_vectors)
+            error("Basis vectors are not linear independent!")
         end
-        return new(quantities, dimensions, dimensional_matrix)
+        return new(basis_vectors, dim_set, dim_mat)
     end
 end
 
-function dimensionless(quantity::Unitful.Quantity, basis_quantities::BasisQuantities)
-
+function unique_dimensions(quantities::Vararg{Unitful.AbstractQuantity})
+    dim_set = Set{Type{<:Unitful.Dimension}}()
+    for quantity in quantities
+        union!(dim_set, typeof.(typeof(dimension(quantity)).parameters[1]))
+    end
+    return dim_set
 end
 
-function dimensionful(quantity::Union{Unitful.NoUnits, Number}, unit::Unitful.Units, basis_quantities::BasisQuantities)
-
+function dimensional_matrix(dim_set::Set{Type{<:Unitful.Dimension}}, quantities::Vararg{Unitful.AbstractQuantity})
+    dim_mat = zeros(Rational, length(dim_set), length(quantities))
+    for (quantity_ind,quantity) in enumerate(quantities)
+        quantity_dims = typeof(typeof(quantity).parameters[2]).parameters[1]
+        for quantity_dim in quantity_dims
+            for (dim_ind,dim) in enumerate(dim_set)
+                if isa(quantity_dim, dim)
+                    dim_mat[dim_ind,quantity_ind] = quantity_dim.power
+                end
+            end
+        end
+    end
+    return dim_mat
 end
+
+function dimensionless(basis::DimensionBasis, quantity::Unitful.AbstractQuantity)
+    dim_mat = dimensional_matrix(basis.dim_set, quantity...)
+    basis_multipliers = prod(basis.basis_vectors.^(basis.dim_mat\dim_mat))
+    return ustrip(uconvert(Unitful.NoUnits, quantity ./ basis_multipliers))
+end
+
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,23 @@
 using Dimensionless, Unitful
 using Test
 
-basis = BasisQuantities(9.81u"m/s^2", 6371u"km", 1420788u"kg", 1u"mol/g")
 
-@testset "Basic tests" begin
-    @test basis.dimensional_matrix == Rational.([-2 0 0 0; 1 1 0 0; 0 0 0 1; 0 0 1 -1])
+basis = DimensionBasis(9.81u"m/s^2", 6371u"km", 1420788u"kg", 1u"mol/g")
+@show dimensionless(basis, 9.81u"m/s^2")
+# dimensionless(basis, 9.81u"m/s^2", 18u"cm/s^2")
+
+@testset "DimensionBasis tests" begin
+    @test_throws ErrorException DimensionBasis(9.81u"m/s^2", 1u"mm", 1u"s")
+    @test_throws ErrorException DimensionBasis(9.81u"m/s^2")
+
+    basis = DimensionBasis(9.81u"m/s^2", 6371u"km", 1420788u"kg", 1u"mol/g")
+    @test basis.dim_mat == Rational.([-2 0 0 0; 1 1 0 0; 0 0 0 1; 0 0 1 -1])
+    for dim in [:Mass, :Length, :Time, :Amount]
+        @test Unitful.Dimension{dim} in basis.dim_set
+    end
+    for dim in [:Temperature, :Current, :Luminosity]
+        @test !(Unitful.Dimension{dim} in basis.dim_set)
+    end
+
+
 end
