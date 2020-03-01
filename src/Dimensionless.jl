0a5a2c239c9780710106f741af7fee2d60fe13e6
Martin Kosch <martin.kosch@rwth-aachen.de>
2020-03-01 18:18:24 +0100

    Changed dimension set to unique dimension array and added dimensionful()

--- src/Dimensionless.jl
+++ src/Dimensionless.jl
@@ -2,54 +2,64 @@ module Dimensionless
 
 using Unitful, LinearAlgebra
 
-export DimensionBasis, dimensional_matrix, dimensionless, dimensionful
+export DimensionBasis, dimensional_matrix, unique_dimensions, dimensionless, dimensionful
 
 struct DimensionBasis
     basis_vectors
-    dim_set
+    basis_dims
     dim_mat
 
     function DimensionBasis(basis_vectors::Vararg{Unitful.AbstractQuantity})
-        dim_set = unique_dimensions(basis_vectors...)
-        dim_mat = dimensional_matrix(dim_set, basis_vectors...)
-
+        basis_dims = unique_dimensions(basis_vectors...)
+        @show eltype(basis_dims)
+        @show typeof(basis_dims) <: Array{Type{<:Unitful.Dimension}}
+        dim_mat = dimensional_matrix(basis_dims, basis_vectors...)
         if size(dim_mat, 2) < size(dim_mat, 1)
             error("Too few basis vectors!")
         elseif LinearAlgebra.rank(dim_mat) != length(basis_vectors)
             error("Basis vectors are not linear independent!")
         end
-        return new(basis_vectors, dim_set, dim_mat)
+        return new(basis_vectors, basis_dims, dim_mat)
     end
 end
 
-function unique_dimensions(quantities::Vararg{Unitful.AbstractQuantity})
-    dim_set = Set{Type{<:Unitful.Dimension}}()
-    for quantity in quantities
-        union!(dim_set, typeof.(typeof(dimension(quantity)).parameters[1]))
+function unique_dimensions(all_dimensions::Vararg{Unitful.Dimensions})
+    basis_dims = Vector{Type{<:Unitful.Dimension}}()
+    for dimensions in all_dimensions
+        union!(basis_dims, typeof.(typeof(dimensions).parameters[1]))
     end
-    return dim_set
+    return basis_dims
 end
 
-function dimensional_matrix(dim_set::Set{Type{<:Unitful.Dimension}}, quantities::Vararg{Unitful.AbstractQuantity})
-    dim_mat = zeros(Rational, length(dim_set), length(quantities))
-    for (quantity_ind,quantity) in enumerate(quantities)
-        quantity_dims = typeof(typeof(quantity).parameters[2]).parameters[1]
-        for quantity_dim in quantity_dims
-            for (dim_ind,dim) in enumerate(dim_set)
-                if isa(quantity_dim, dim)
-                    dim_mat[dim_ind,quantity_ind] = quantity_dim.power
-                end
+unique_dimensions(all_quantities::Vararg{Unitful.AbstractQuantity}) =
+    unique_dimensions(dimension.(all_quantities)...)
+
+function dimensional_matrix(basis_dims::Array{Type{<:Unitful.Dimension}}, all_dimensions::Vararg{Unitful.Dimensions})
+    dim_mat = zeros(Rational, length(basis_dims), length(all_dimensions))
+    for (dimension_ind,dimensions) in enumerate(all_dimensions)
+        for dimension in typeof(dimensions).parameters[1]
+            basis_dim_ind = findfirst(x -> isa(dimension, x), basis_dims)
+            if basis_dim_ind == nothing
+                error("$(typeof(dimension)) is no basis dimension!")
             end
+            dim_mat[basis_dim_ind,dimension_ind] = dimension.power
         end
     end
     return dim_mat
 end
 
+dimensional_matrix(basis_dims::Array{Type{<:Unitful.Dimension}}, all_quantities::Vararg{Unitful.AbstractQuantity}) =
+    dimensional_matrix(basis_dims, dimension.(all_quantities)...)
+
 function dimensionless(basis::DimensionBasis, quantity::Unitful.AbstractQuantity)
-    dim_mat = dimensional_matrix(basis.dim_set, quantity...)
+    dim_mat = dimensional_matrix(basis.basis_dims, quantity...)
     basis_multipliers = prod(basis.basis_vectors.^(basis.dim_mat\dim_mat))
     return ustrip(uconvert(Unitful.NoUnits, quantity ./ basis_multipliers))
 end
 
+function dimensionful(basis::DimensionBasis, value::Number, unit::Unitful.Units)
+    dim_vec = dimensional_matrix(basis.basis_dims, dimension(unit))
+    return uconvert.(unit, value .* prod(basis.basis_vectors.^(basis.dim_mat\dim_vec)))
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,16 +1,13 @@
 using Dimensionless, Unitful
 using Test
 
-
-basis = DimensionBasis(9.81u"m/s^2", 6371u"km", 1420788u"kg", 1u"mol/g")
-@show dimensionless(basis, 9.81u"m/s^2")
-# dimensionless(basis, 9.81u"m/s^2", 18u"cm/s^2")
-
 @testset "DimensionBasis tests" begin
     @test_throws ErrorException DimensionBasis(9.81u"m/s^2", 1u"mm", 1u"s")
     @test_throws ErrorException DimensionBasis(9.81u"m/s^2")
 
     basis = DimensionBasis(9.81u"m/s^2", 6371u"km", 1420788u"kg", 1u"mol/g")
+    @test_throws ErrorException dimensional_matrix(basis.basis_dims, 1u"A")
+
     @test basis.dim_mat == Rational.([-2 0 0 0; 1 1 0 0; 0 0 0 1; 0 0 1 -1])
     for dim in [:Mass, :Length, :Time, :Amount]
         @test Unitful.Dimension{dim} in basis.dim_set
@@ -18,6 +15,5 @@ basis = DimensionBasis(9.81u"m/s^2", 6371u"km", 1420788u"kg", 1u"mol/g")
     for dim in [:Temperature, :Current, :Luminosity]
         @test !(Unitful.Dimension{dim} in basis.dim_set)
     end
-
 
 end
