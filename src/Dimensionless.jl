72b6585a1ef2972dff04d458a5aba2d83727f667
Martin Kosch <martin.kosch@rwth-aachen.de>
2020-03-01 20:56:05 +0100

    Basis change and helper functions implemented

--- src/Dimensionless.jl
+++ src/Dimensionless.jl
@@ -2,7 +2,7 @@ module Dimensionless
 
 using Unitful, LinearAlgebra
 
-export DimensionBasis, dimensional_matrix, unique_dimensions, dimensionless, dimensionful
+export DimensionBasis, dimensional_matrix, dimensionless, dimensionful, old_to_new_multiplier, change_basis
 
 struct DimensionBasis
     basis_vectors
@@ -11,9 +11,8 @@ struct DimensionBasis
 
     function DimensionBasis(basis_vectors::Vararg{Unitful.AbstractQuantity})
         basis_dims = unique_dimensions(basis_vectors...)
-        @show eltype(basis_dims)
-        @show typeof(basis_dims) <: Array{Type{<:Unitful.Dimension}}
         dim_mat = dimensional_matrix(basis_dims, basis_vectors...)
+
         if size(dim_mat, 2) < size(dim_mat, 1)
             error("Too few basis vectors!")
         elseif LinearAlgebra.rank(dim_mat) != length(basis_vectors)
@@ -22,6 +21,8 @@ struct DimensionBasis
         return new(basis_vectors, basis_dims, dim_mat)
     end
 end
+
+Base.Broadcast.broadcastable(basis::DimensionBasis) = Ref(basis)
 
 function unique_dimensions(all_dimensions::Vararg{Unitful.Dimensions})
     basis_dims = Vector{Type{<:Unitful.Dimension}}()
@@ -48,18 +49,33 @@ function dimensional_matrix(basis_dims::Array{Type{<:Unitful.Dimension}}, all_di
     return dim_mat
 end
 
-dimensional_matrix(basis_dims::Array{Type{<:Unitful.Dimension}}, all_quantities::Vararg{Unitful.AbstractQuantity}) =
+dimensional_matrix(basis_dims::Array{Type{<:Unitful.Dimension}}, all_quantities::Vararg{Union{Unitful.AbstractQuantity, Unitful.Units}}) =
     dimensional_matrix(basis_dims, dimension.(all_quantities)...)
 
-function dimensionless(basis::DimensionBasis, quantity::Unitful.AbstractQuantity)
-    dim_mat = dimensional_matrix(basis.basis_dims, quantity...)
-    basis_multipliers = prod(basis.basis_vectors.^(basis.dim_mat\dim_mat))
-    return ustrip(uconvert(Unitful.NoUnits, quantity ./ basis_multipliers))
+dimensional_matrix(all_dimensions::Vararg{Unitful.Dimensions}) = dimensional_matrix(unique_dimensions(all_dimensions...), all_dimensions...)
+
+dimensional_matrix(all_quantities::Vararg{Union{Unitful.AbstractQuantity, Unitful.Units}}) = dimensional_matrix(unique_dimensions(dimension.(all_quantities)...), dimension.(all_quantities)...)
+
+function dimensionless(quantity::Unitful.AbstractQuantity, basis::DimensionBasis)
+    dim_mat = dimensional_matrix(basis.basis_dims, quantity)
+    basis_multipliers = prod(basis.basis_vectors .^ (basis.dim_mat \ dim_mat))
+    return ustrip(uconvert(Unitful.NoUnits, quantity / basis_multipliers))
 end
 
-function dimensionful(basis::DimensionBasis, value::Number, unit::Unitful.Units)
+function dimensionful(value::Number, unit::Unitful.Units, basis::DimensionBasis)
     dim_vec = dimensional_matrix(basis.basis_dims, dimension(unit))
-    return uconvert.(unit, value .* prod(basis.basis_vectors.^(basis.dim_mat\dim_vec)))
+    return uconvert(unit, value * prod(basis.basis_vectors .^ (basis.dim_mat \ dim_vec)))
 end
+
+function old_to_new_multiplier(dimensions::Unitful.Dimensions, old_basis::DimensionBasis, new_basis::DimensionBasis)
+    old_dim_mat = dimensional_matrix(old_basis.basis_dims, dimensions)
+    new_dim_mat = dimensional_matrix(new_basis.basis_dims, dimensions)
+    multiplier = prod(new_basis.basis_vectors .^ (new_basis.dim_mat \ new_dim_mat)) / prod(old_basis.basis_vectors .^ (old_basis.dim_mat \ old_dim_mat))
+    return ustrip(uconvert(Unitful.NoUnits, multiplier))
+end
+
+old_to_new_multiplier(quantity::Union{Unitful.AbstractQuantity, Unitful.Units}, old_basis::DimensionBasis, new_basis::DimensionBasis) = old_to_new_multiplier(dimension(quantity), old_basis, new_basis)
+
+change_basis(quantity::Unitful.AbstractQuantity, old_basis::DimensionBasis, new_basis::DimensionBasis) = quantity * old_to_new_multiplier(quantity, old_basis, new_basis)
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -8,12 +8,18 @@ using Test
     basis = DimensionBasis(9.81u"m/s^2", 6371u"km", 1420788u"kg", 1u"mol/g")
     @test_throws ErrorException dimensional_matrix(basis.basis_dims, 1u"A")
 
-    @test basis.dim_mat == Rational.([-2 0 0 0; 1 1 0 0; 0 0 0 1; 0 0 1 -1])
+    @test basis.dim_mat == Rational.([1 1 0 0; -2 0 0 0; 0 0 1 -1; 0 0 0 1])
     for dim in [:Mass, :Length, :Time, :Amount]
-        @test Unitful.Dimension{dim} in basis.dim_set
+        @test Unitful.Dimension{dim} in basis.basis_dims
     end
     for dim in [:Temperature, :Current, :Luminosity]
-        @test !(Unitful.Dimension{dim} in basis.dim_set)
+        @test !(Unitful.Dimension{dim} in basis.basis_dims)
     end
+end
 
+@testset "Change of Basis tests" begin
+    dimensional_matrix(1u"mA", 2u"mol", 3u"Pa*s", 4u"h", 5u"kg")
+    basis = DimensionBasis(1u"mA", 2u"mol", 3u"Pa*s", 4u"h", 5u"kg")
+    dim_less = dimensionless(1u"m/s^2", basis)
+    @test dimensionful(dim_less, u"m/s^2", basis) == 1u"m/s^2"
 end
