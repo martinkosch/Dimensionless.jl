41fe1136f2fcb3a00dad81ea1ca4f4a058f4a6a2
Martin Kosch <martin.kosch@rwth-aachen.de>
2020-03-04 19:52:18 +0100

    Shorted names

--- examples/examples.jl
+++ examples/examples.jl
@@ -7,4 +7,18 @@ change_basis(1600u"N*m", applications_basis, model_basis)
 applications_basis  =    dimension_basis(1u"m",      1028u"kg/m^3",  1.88e-3u"Pa*s")
 model_basis         =    dimension_basis(0.025u"m",   998u"kg/m^3",   1e-3u"Pa*s")
 change_basis(5u"m/s", applications_basis, model_basis)
-old_to_new_multiplier(u"N", model_basis, applications_basis)
+change_basis(u"N", model_basis, applications_basis)
+
+
+
+function empiric_barometric(h)
+    a0 = 7.001985e-02
+    a1 = -4.336216e-03
+    a2 = -5.009831e-03
+    a3 = 1.621827e-04
+    a4 = -2.471283e-06
+    a5 = 1.904383e-08
+    a6 = -7.189421e-11
+    a7 = 1.060067e-13
+    return 10^(((((((a7*h/1000 + a6)*h/1000 + a5)*h/1000 + a4)*h/1000 + a3)*h/1000 + a2)*h/1000 + a1)*h/1000 + a0)
+end
--- src/Dimensionless.jl
+++ src/Dimensionless.jl
@@ -2,61 +2,61 @@ module Dimensionless
 
 using Unitful, LinearAlgebra
 
-export dimension_basis, dimensional_matrix, dimensional_matrix_spy, dimensionless, dimensionful, old_to_new_multiplier, change_basis
+export dim_basis, dim_matrix, dim_matrix_spy, dimensionless, dimensionful, change_basis
 
-abstract type AbstractDimensionBasis end
+abstract type AbstractDimBasis end
 
-Base.Broadcast.broadcastable(basis::AbstractDimensionBasis) = Ref(basis)
+Base.Broadcast.broadcastable(basis::AbstractDimBasis) = Ref(basis)
 
-struct UnnamedDimensionBasis <: AbstractDimensionBasis
+struct UnnamedDimBasis <: AbstractDimBasis
     basis_vectors
     basis_dims
     dim_mat
 
-    function UnnamedDimensionBasis(basis_vectors::Vararg{Unitful.AbstractQuantity})
-        basis_dims = unique_dimensions(basis_vectors...)
-        dim_mat = dimensional_matrix(basis_dims, basis_vectors...)
+    function UnnamedDimBasis(basis_vectors::Vararg{Unitful.AbstractQuantity})
+        basis_dims = unique_dims(basis_vectors...)
+        dim_mat = dim_matrix(basis_dims, basis_vectors...)
         check_basis(dim_mat)
         return new(collect(basis_vectors), basis_dims, dim_mat)
     end
 end
 
-struct NamedDimensionBasis <: AbstractDimensionBasis
+struct NamedDimBasis <: AbstractDimBasis
     basis_vectors
     basis_vector_names
     basis_dims
     dim_mat
 
-    function NamedDimensionBasis(named_basis_vectors::Vararg{Pair{String,<:Unitful.AbstractQuantity}})
+    function NamedDimBasis(named_basis_vectors::Vararg{Pair{String,<:Unitful.AbstractQuantity}})
         basis_vector_names = [named_basis_vector.first for named_basis_vector in named_basis_vectors]
         basis_vectors = [named_basis_vector.second for named_basis_vector in named_basis_vectors]
-        basis_dims = unique_dimensions(basis_vectors...)
-        dim_mat = dimensional_matrix(basis_dims, basis_vectors...)
+        basis_dims = unique_dims(basis_vectors...)
+        dim_mat = dim_matrix(basis_dims, basis_vectors...)
         check_basis(dim_mat)
         return new(basis_vectors, basis_vector_names, basis_dims, dim_mat)
     end
 end
 
-dimension_basis(basis_vectors::Vararg{Unitful.AbstractQuantity}) = UnnamedDimensionBasis(basis_vectors...)
+dim_basis(basis_vectors::Vararg{Unitful.AbstractQuantity}) = UnnamedDimBasis(basis_vectors...)
 
-dimension_basis(named_basis_vectors::Vararg{Pair{String,<:Unitful.AbstractQuantity}}) = NamedDimensionBasis(named_basis_vectors...)
+dim_basis(named_basis_vectors::Vararg{Pair{String,<:Unitful.AbstractQuantity}}) = NamedDimBasis(named_basis_vectors...)
 
 
-function unique_dimensions(all_dimensions::Vararg{Unitful.Dimensions})
+function unique_dims(all_dims::Vararg{Unitful.Dimensions})
     basis_dims = Vector{Type{<:Unitful.Dimension}}()
-    for dimensions in all_dimensions
-        union!(basis_dims, typeof.(typeof(dimensions).parameters[1]))
+    for dims in all_dims
+        union!(basis_dims, typeof.(typeof(dims).parameters[1]))
     end
     return basis_dims
 end
 
-unique_dimensions(all_quantities::Vararg{Unitful.AbstractQuantity}) =
-    unique_dimensions(dimension.(all_quantities)...)
+unique_dims(all_quantities::Vararg{Unitful.AbstractQuantity}) =
+    unique_dims(dimension.(all_quantities)...)
 
-function dimensional_matrix(basis_dims::Array{Type{<:Unitful.Dimension}}, all_dimensions::Vararg{Unitful.Dimensions})
-    dim_mat = zeros(Rational, length(basis_dims), length(all_dimensions))
-    for (dimension_ind,dimensions) in enumerate(all_dimensions)
-        for dimension in typeof(dimensions).parameters[1]
+function dim_matrix(basis_dims::Array{Type{<:Unitful.Dimension}}, all_dims::Vararg{Unitful.Dimensions})
+    dim_mat = zeros(Rational, length(basis_dims), length(all_dims))
+    for (dimension_ind,dims) in enumerate(all_dims)
+        for dimension in typeof(dims).parameters[1]
             basis_dim_ind = findfirst(x -> isa(dimension, x), basis_dims)
             if basis_dim_ind == nothing
                 error("$(typeof(dimension)) is no basis dimension!")
@@ -67,12 +67,12 @@ function dimensional_matrix(basis_dims::Array{Type{<:Unitful.Dimension}}, all_di
     return dim_mat
 end
 
-dimensional_matrix(basis_dims::Array{Type{<:Unitful.Dimension}}, all_quantities::Vararg{Union{Unitful.AbstractQuantity, Unitful.Units}}) =
-    dimensional_matrix(basis_dims, dimension.(all_quantities)...)
+dim_matrix(basis_dims::Array{Type{<:Unitful.Dimension}}, all_quantities::Vararg{Union{Unitful.AbstractQuantity, Unitful.Units}}) =
+    dim_matrix(basis_dims, dimension.(all_quantities)...)
 
-dimensional_matrix_spy(all_dimensions::Vararg{Unitful.Dimensions}) = dimensional_matrix(unique_dimensions(all_dimensions...), all_dimensions...)
+dim_matrix_spy(all_dims::Vararg{Unitful.Dimensions}) = dim_matrix(unique_dims(all_dims...), all_dims...)
 
-dimensional_matrix_spy(all_quantities::Vararg{Union{Unitful.AbstractQuantity, Unitful.Units}}) = dimensional_matrix(unique_dimensions(dimension.(all_quantities)...), dimension.(all_quantities)...)
+dim_matrix_spy(all_quantities::Vararg{Union{Unitful.AbstractQuantity, Unitful.Units}}) = dim_matrix(unique_dims(dimension.(all_quantities)...), dimension.(all_quantities)...)
 
 function check_basis(dim_mat)
     if size(dim_mat, 2) < size(dim_mat, 1)
@@ -88,26 +88,26 @@ function check_basis(dim_mat)
     end
 end
 
-function dimensionless(quantity::Unitful.AbstractQuantity, basis::AbstractDimensionBasis)
-    dim_mat = dimensional_matrix(basis.basis_dims, quantity)
+function dimensionless(quantity::Unitful.AbstractQuantity, basis::AbstractDimBasis)
+    dim_mat = dim_matrix(basis.basis_dims, quantity)
     basis_multipliers = prod(basis.basis_vectors .^ (basis.dim_mat \ dim_mat))
     return ustrip(uconvert(Unitful.NoUnits, quantity / basis_multipliers))
 end
 
-function dimensionful(value::Number, unit::Unitful.Units, basis::AbstractDimensionBasis)
-    dim_vec = dimensional_matrix(basis.basis_dims, dimension(unit))
+function dimensionful(value::Number, unit::Unitful.Units, basis::AbstractDimBasis)
+    dim_vec = dim_matrix(basis.basis_dims, dimension(unit))
     return uconvert(unit, value * prod(basis.basis_vectors .^ (basis.dim_mat \ dim_vec)))
 end
 
-function old_to_new_multiplier(dimensions::Unitful.Dimensions, old_basis::AbstractDimensionBasis, new_basis::AbstractDimensionBasis)
-    old_dim_mat = dimensional_matrix(old_basis.basis_dims, dimensions)
-    new_dim_mat = dimensional_matrix(new_basis.basis_dims, dimensions)
+function old_to_new_multiplier(dims::Unitful.Dimensions, old_basis::AbstractDimBasis, new_basis::AbstractDimBasis)
+    old_dim_mat = dim_matrix(old_basis.basis_dims, dims)
+    new_dim_mat = dim_matrix(new_basis.basis_dims, dims)
     multiplier = prod(new_basis.basis_vectors .^ (new_basis.dim_mat \ new_dim_mat)) / prod(old_basis.basis_vectors .^ (old_basis.dim_mat \ old_dim_mat))
     return ustrip(uconvert(Unitful.NoUnits, multiplier))
 end
 
-old_to_new_multiplier(quantity::Union{Unitful.AbstractQuantity, Unitful.Units}, old_basis::AbstractDimensionBasis, new_basis::AbstractDimensionBasis) = old_to_new_multiplier(dimension(quantity), old_basis, new_basis)
+change_basis(quantity::Unitful.AbstractQuantity, old_basis::AbstractDimBasis, new_basis::AbstractDimBasis) = quantity * old_to_new_multiplier(dimension(quantity), old_basis, new_basis)
 
-change_basis(quantity::Unitful.AbstractQuantity, old_basis::AbstractDimensionBasis, new_basis::AbstractDimensionBasis) = quantity * old_to_new_multiplier(quantity, old_basis, new_basis)
+change_basis(unit::Unitful.Units, old_basis::AbstractDimBasis, new_basis::AbstractDimBasis) = old_to_new_multiplier(dimension(unit), old_basis, new_basis)
 
 end # module
